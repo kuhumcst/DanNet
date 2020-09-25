24d84cdb8bd4e7ce50c672c053148a0d8dc4059d
Simon Gray <simongray@gmail.com>
2020-09-25 16:29:03 +0200

    datafy neo4j query results

--- src/dannet/db/neo4j.clj
+++ src/dannet/db/neo4j.clj
@@ -1,8 +1,14 @@
 (ns dannet.db.neo4j
   (:require [clojure.java.io :as io]
+            [clojure.datafy :refer [datafy]]
+            [clojure.core.protocols :as p]
+            [clojure.reflect :refer [reflect]]              ; for REPL use
             [neo4j-clj.core :as neo4j]
-            [dannet.io :as dio])
-  (:import [java.net URI]))
+            [dannet.io :as dio]
+            [clojure.string :as str])
+  (:import [java.net URI]
+           [org.neo4j.driver.types Relationship Path Entity]
+           [org.neo4j.driver QueryRunner Result Record]))
 
 (defn fetch-path!
   "Fetch the `path` with a specific serialisation `fmt` in the given db `sess`."
@@ -25,6 +31,55 @@
     (let [folder (io/file (io/resource source))
           source (dio/source-folder folder path-fn)]
       (import-rdf! session source))))
+
+(defn q
+  "Alternative to the execute function that doesn't autoconvert results."
+  [^QueryRunner sess ^String cypher]
+  (.run sess cypher))
+
+(defn- keywordize
+  [s]
+  (let [[ns k] (str/split s #"__")]
+    (if k
+      (keyword ns k)
+      (keyword s))))
+
+;; Neo4j query results are made Datafiable.
+;; The `neo4j-clj.compability/neo4->clj` fn both loses valuable data (e.g. ids)
+;; and doesn't convert all types of data (e.g. Path). The fact that the
+;; conversion is forced in `neo4j-clj.core/execute` also needlessly cuts the
+;; link to the underlying Java objects.
+(extend-protocol p/Datafiable
+  Result
+  (datafy [result]
+    (mapv datafy (doall (iterator-seq result))))
+
+  Record
+  (datafy [record]
+    (into {} (for [[k v] (.asMap record)]
+               [(keywordize k) (datafy v)])))
+
+  Entity
+  (datafy [entity]
+    {:id         (.id entity)
+     :properties (into {} (for [[k v] (.asMap entity)]
+                            [(keywordize k) (datafy v)]))})
+
+  Relationship
+  (datafy [rel]
+    {:id         (.id rel)
+     :start      (.startNodeId rel)
+     :end        (.endNodeId rel)
+     :type       (keywordize (.type rel))
+     :properties (datafy (.asMap rel))})
+
+  Path
+  (datafy [path]
+    {:start         (datafy (.start path))
+     :end           (datafy (.end path))
+     :nodes         (mapv datafy (.nodes path))
+     :relationships (mapv datafy (.relationships path))}))
+
 
 (comment
   ;; https://github.com/gorillalabs/neo4j-clj
@@ -55,4 +110,13 @@
         source        "dannet/rdf"]
     (load-graph! conn "dannet/rdf" (partial ->docker-path "dannet/rdf"))
     (load-graph! conn "wordnet/rdf" (partial ->docker-path "wordnet/rdf")))
+
+  ;; Match against a subgraph and datafy the result.
+  (neo4j/with-transaction conn tx
+    (->> "MATCH p=()--()--()--({ns1__lexicalForm: 'computer'})
+          RETURN p
+          LIMIT 100"
+         (q tx)
+         (datafy)))
   #_.)
+
