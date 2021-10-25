a54108ebb98d7ada2d3425dd980ebf0c963eccb6
Simon Gray <simongray@gmail.com>
2021-10-25 16:41:32 +0200

    added local caches for lemon and lexinfo
    (networks requests were timing out)

--- src/main/dk/wordnet/prefix.clj
+++ src/main/dk/wordnet/prefix.clj
@@ -8,12 +8,14 @@
   {'wn      {:uri "https://globalwordnet.github.io/schemas/wn#"
              :alt (str (io/resource "schemas/wn-lemon-1.2.ttl"))}
    'ontolex {:uri "http://www.w3.org/ns/lemon/ontolex#"}
-   'lemon   {:uri "http://lemon-model.net/lemon#"}
+   'lemon   {:uri "http://lemon-model.net/lemon#"
+             :alt (str (io/resource "schemas/lemon-model.ttl"))}
    'semowl  {:uri "http://www.ontologydesignpatterns.org/cp/owl/semiotics.owl#"
              :alt (str (io/resource "schemas/semiotics.owl"))}
    'skos    {:uri "http://www.w3.org/2004/02/skos/core#"
              :alt "http://www.w3.org/TR/skos-reference/skos.rdf"}
-   'lexinfo {:uri "http://www.lexinfo.net/ontology/3.0/lexinfo#"}
+   'lexinfo {:uri "http://www.lexinfo.net/ontology/3.0/lexinfo#"
+             :alt (str (io/resource "schemas/lexinfo-3.0.owl"))}
    'dn      {:uri          "http://www.wordnet.dk/dannet/2022/instances/"
              :instance-ns? true}
    'dns     {:uri "http://www.wordnet.dk/dannet/2022/schema/"
