0542c7310e6d59b75afa0dd9eb7d70afb9862794
Simon Gray <simongray@gmail.com>
2024-05-27 15:32:54 +0200

    fixed WN-LMF export
    - fetch wn relations separately, much faster
    - fix sense/synset relation bug in lexical entry query

--- src/main/dk/cst/dannet/db/export/wn_lmf.clj
+++ src/main/dk/cst/dannet/db/export/wn_lmf.clj
@@ -10,33 +10,136 @@
             [dk.cst.dannet.query :as q]
             [dk.cst.dannet.query.operation :as op]))
 
+(def wn-relations
+  "Taken from https://globalwordnet.github.io/schemas/"
+  [:wn/hypernym
+   :wn/hyponym
+   :wn/instance_hyponym
+   :wn/instance_hypernym
+   :wn/mero_member
+   :wn/holo_member
+   :wn/mero_part
+   :wn/holo_part
+   :wn/mero_substance
+   :wn/holo_substance
+   :wn/entails
+   :wn/causes
+   :wn/similar
+   :wn/also
+   :wn/attribute
+   :wn/domain_topic
+   :wn/has_domain_topic
+   :wn/domain_region
+   :wn/has_domain_region
+   :wn/exemplifies
+   :wn/is_exemplified_by
+   :wn/agent
+   :wn/antonym
+   :wn/be_in_state
+   :wn/classified_by
+   :wn/classifies
+   :wn/co_agent_instrument
+   :wn/co_agent_patient
+   :wn/co_agent_result
+   :wn/co_instrument_agent
+   :wn/co_instrument_patient
+   :wn/co_instrument_result
+   :wn/co_patient_agent
+   :wn/co_patient_instrument
+   :wn/co_result_agent
+   :wn/co_result_instrument
+   :wn/co_role
+   :wn/direction
+   :wn/eq_synonym
+   :wn/holo_location
+   :wn/holo_portion
+   :wn/holonym
+   :wn/in_manner
+   :wn/instrument
+   :wn/involved_agent
+   :wn/involved_direction
+   :wn/involved_instrument
+   :wn/involved_location
+   :wn/involved_patient
+   :wn/involved_result
+   :wn/involved_source_direction
+   :wn/involved_target_direction
+   :wn/involved
+   :wn/is_caused_by
+   :wn/is_entailed_by
+   :wn/is_subevent_of
+   :wn/location
+   :wn/manner_of
+   :wn/mero_location
+   :wn/mero_portion
+   :wn/meronym
+   :wn/other
+   :wn/patient
+   :wn/restricted_by
+   :wn/restricts
+   :wn/result
+   :wn/role
+   :wn/source_direction
+   :wn/state_of
+   :wn/subevent
+   :wn/target_direction])
+
 (def dannet-graph
   (delay (let [dataset (:dataset @dk.cst.dannet.web.resources/db)]
            (db/get-graph dataset prefix/dn-uri))))
 
+(defn ->wn-relations-query
+  [rel]
+  (op/sparql
+    "SELECT ?subject ?object
+     WHERE {
+       ?subject " (prefix/kw->qname rel) " ?object .
+     }"))
+
 (def lexical-entry-query
   (op/sparql
-    "SELECT ?lexicalEntry ?synset ?sense ?pos ?writtenRep ?example ?ili ?definition
+    "SELECT ?lexicalEntry ?synset ?sense ?pos ?writtenRep
      WHERE {
        ?lexicalEntry ontolex:evokes ?synset ;
                      ontolex:sense ?sense ;
                      wn:partOfSpeech ?pos ;
                      ontolex:canonicalForm ?form .
 
-       ?form ontolex:writtenRep ?writtenRep .
+       # Only keep results where parts are interrelated (sense, entry, synset)
+       ?synset ontolex:lexicalizedSense ?sense .
 
-       OPTIONAL { ?synset wn:ili ?ili . }
-       OPTIONAL { ?synset skos:definition ?definition . }
-       OPTIONAL { ?sense lexinfo:senseExample ?example . }
+       ?form ontolex:writtenRep ?writtenRep .
+     }"))
+
+(def sense-example-query
+  (op/sparql
+    "SELECT ?synset ?example
+     WHERE {
+       ?sense lexinfo:senseExample ?example .
+       ?synset ontolex:lexicalizedSense ?sense .
+     }"))
+
+(def ili-query
+  (op/sparql
+    "SELECT ?synset ?ili
+     WHERE {
+       ?synset wn:ili ?ili .
+     }"))
+
+(def definition-query
+  (op/sparql
+    "SELECT ?synset ?definition
+     WHERE {
+       ?synset skos:definition ?definition .
      }"))
 
 (defn ->synset-relations-query
-  [synset]
+  [?synset]
   (op/sparql
-    "SELECT ?rel ?object
+    "SELECT ?synset ?rel ?object
      WHERE {
-       " (prefix/kw->qname synset) " ?rel ?object .
        ?object a ontolex:LexicalConcept .
+       " (prefix/kw->qname ?synset) " ?rel ?object .
      }"))
 
 (def pos-str
@@ -45,39 +148,37 @@
    :wn/verb      "v"})
 
 (defn lexical-entry
-  [[id vs]]
+  [[id ms]]
   (into [:LexicalEntry {:id (name id)}
-         [:Lemma {:writtenForm  (-> vs first (get '?writtenRep) str)
-                  :partOfSpeech (-> vs first (get '?pos) pos-str)}]]
-        (map (fn [{:syms [?sense ?synset]}]
-               [:Sense {:id     (name ?sense)
-                        :synset (name ?synset)}])
-             vs)))
+         [:Lemma {:writtenForm  (-> ms first (get '?writtenRep) str)
+                  :partOfSpeech (-> ms first (get '?pos) pos-str)}]]
+        (mapcat (fn [[synset ms]]
+                  (for [{:syms [?sense]} ms]
+                    [:Sense {:id     (name ?sense)
+                             :synset (name synset)}]))
+                (->> (group-by '?synset ms)
+                     (sort-by first)))))
 
 (defn synset-relations
-  [g synset]
-  (->> (q/run g (->synset-relations-query synset))
-       (filter (comp #{"wn"} namespace '?rel))
-       (map (fn [{:syms [?rel ?object]}]
-              [:SynsetRelation {:relType (name ?rel)
-                                :target  (name ?object)}]))))
+  [ms]
+  (for [{:syms [?rel ?object]} ms]
+    [:SynsetRelation {:relType (name ?rel)
+                      :target  (name ?object)}]))
 
 (defn synset
-  [g [id vs]]
-  (let [pos     (-> vs first (get '?pos) pos-str)
-        ili     (-> vs first (get '?ili))
-        members (str/join " " (map (comp name '?sense) vs))]
+  [synset-props [id ms]]
+  (let [pos        (-> ms first (get '?pos) pos-str)
+        {:keys [ili definition examples members]} (get synset-props id)]
     (into [:Synset (cond-> {:id      (name id)
-                            :members members}
+                            :members (str/join " " members)}
                      pos (assoc :partOfSpeech pos)
-                     ili (assoc :ili (name ili)))
-           (when-let [definition (some-> vs first (get '?definition) str)]
+                     ili (assoc :ili ili))
+           (when definition
              [:Definition definition])]
           (concat
-            (->> (keep '?example vs)
-                 (map (fn [example]
-                        [:Example (str example)])))
-            (synset-relations g id)))))
+            (for [example examples]
+              [:Example (str example)])
+            (synset-relations ms)))))
 
 (def lexicon
   [:Lexicon
@@ -91,14 +192,12 @@
     :url      "https://wordnet.dk/dannet"}])
 
 (defn lexical-resource
-  [g ms]
+  [[lexical-entry-results relation-results synset-props]]
   [:LexicalResource {:xmlns/dc "https://globalwordnet.github.io/schemas/dc/"}
    (into lexicon
          (concat
-           (->> (group-by '?lexicalEntry ms)
-                (map lexical-entry))
-           (->> (group-by '?synset ms)
-                (map (partial synset g)))))])
+           (map lexical-entry lexical-entry-results)
+           (map #(synset synset-props %) relation-results)))])
 
 (def doctype
   "<!DOCTYPE LexicalResource SYSTEM \"http://globalwordnet.github.io/schemas/WN-LMF-1.1.dtd\">")
@@ -108,19 +207,79 @@
   (let [[before after] (str/split xml #"\r?\n" 2)]
     (str before "\n" doctype "\n" after)))
 
-(defn xml-str
+(defmacro label-time
+  "Evaluates expr and prints the time it took.  Returns the value of
+ expr."
+  {:added "1.0"}
+  [label expr]
+  `(let [start# (. System (nanoTime))
+         nada#  (print ~label)
+         ret#   ~expr]
+     (println (str " ... " (/ (double (- (. System (nanoTime)) start#)) 1000000000.0) " secs"))
+     ret#))
+
+(defn get-relations
   [g]
-  (let [ms (q/run g lexical-entry-query)]
-    (-> (lexical-resource g ms)
-        (xml/sexp-as-element)
-        (xml/indent-str)
-        (add-doctype))))
+  (loop [out []
+         [rel & rels] wn-relations]
+    (if rel
+      (recur (->> (q/run g (->wn-relations-query rel))
+                  (map #(assoc % '?rel rel))
+                  (concat out))
+             rels)
+      out)))
+
+(defn run-queries
+  [g]
+  (let [lexical-entry-res    (label-time
+                               'lexical-entry-res
+                               (q/run g lexical-entry-query))
+        sense-example-res    (label-time
+                               'sense-example-res
+                               (q/run g sense-example-query))
+        ili-query-res        (label-time
+                               'ili-query-res
+                               (q/run g ili-query))
+        definition-query-res (label-time
+                               'definition-query-res
+                               (q/run g definition-query))
+        get-relations-res    (label-time
+                               'get-relations
+                               (get-relations g))]
+    [(group-by '?lexicalEntry lexical-entry-res)
+     (group-by '?subject get-relations-res)
+     (merge-with merge
+                 (update-vals
+                   (group-by '?synset lexical-entry-res)
+                   (fn [ms]
+                     {:members (sort (set (map (comp name '?sense) ms)))}))
+                 (update-vals
+                   (group-by '?synset sense-example-res)
+                   (fn [ms]
+                     {:examples (sort (set (map (comp str '?example) ms)))}))
+                 (update-vals
+                   (group-by '?synset ili-query-res)
+                   (fn [ms]
+                     {:ili (-> ms first (get '?ili) name)}))
+                 (update-vals
+                   (group-by '?synset definition-query-res)
+                   (fn [ms]
+                     {:definition (-> ms first (get '?definition) str)})))]))
+
+(defn xml-str
+  [[lexical-entry-results relation-results _synset-props :as query-results]]
+  (println (count lexical-entry-results) "lexical entries")
+  (println (count relation-results) "synsets")
+  (-> (lexical-resource query-results)
+      (xml/sexp-as-element)
+      (xml/indent-str)
+      (add-doctype)))
 
 (defn export-xml!
   [f]
   (println "Exporting" f)
   (io/make-parents f)
-  (spit f (xml-str @dannet-graph)))
+  (spit f (xml-str (run-queries @dannet-graph))))
 
 (defn export-wn-lmf!
   "Export DanNet into `dir` as WordNet LMF."
@@ -135,12 +294,7 @@
   (println "WN-LMF export of DanNet complete!"))
 
 (comment
-  (->> (q/run @dannet-graph lexical-entry-query)
-       (group-by (comp name '?lexicalEntry))
-       (map lexical-entry))
-
-  ;; the full dataset takes ~20 minutes to write (expensive synset queries)
-  (time (xml-str @dannet-graph))
+  (xml-str (run-queries @dannet-graph))
   (time (export-xml! "export/wn-lmf/dannet-wn-lmf.xml"))
   (time (export-wn-lmf! "export/wn-lmf/"))
   #_.)
